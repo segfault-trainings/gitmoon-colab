cb6998991bc547b35c312777c6b3b5f2516ca8ea
fliiiix <hi@l33t.name>
2022-03-22 14:30:32 +0000

    Deploying to gh-pages from @ segfault-trainings/gitmoon-collab@fc1afc6798d02a51f7527d9cd29ed09be74bed9e 🚀

--- __tmp.js
+++ __tmp.js
@@ -1,1 +1,5 @@
+new Moon("#9999ff", "Moon :) ");new Moon("yellow", "yellow");
 new Moon("pink", "<NAME>")new Moon("fuchsia", "I am a Moon 🌚");
+new Moon("#EAD2A8", "I am koma");
+new Moon("cyan", "I am a Moon 🌚");
+new Moon("#f7a3e4", "Pascal's Moon🌛");
