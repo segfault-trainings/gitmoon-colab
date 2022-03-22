ecbc2ec3fcac7ed3ff3cb307585e5002c0ad14fb
randombenj <randombenj@gmail.com>
2022-03-22 14:37:58 +0000

    Deploying to gh-pages from @ segfault-trainings/gitmoon-collab@7c4ec36592e6b80678526882a1f84c1a1ab29e7a 🚀

--- __tmp.js
+++ __tmp.js
@@ -4,6 +4,7 @@ new Moon("yellow", "yellow");
 new Moon("pink", "<NAME>");
 new Moon("fuchsia", "I am a Moon 🌚");
 new Moon("#EAD2A8", "I am koma");
+new Moon("white", "first");
 new Moon("orange", "Mond 🌚");
 new Moon("cyan", "I am a Moon 🌚");
 new Moon("#f7a3e4", "Pascal's Moon🌛");
